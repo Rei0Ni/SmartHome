4f21e534f687cb6492b768f6e1af78ee5efb0313
Rei0Ni <abdelrahman.hamdy.hashim@gmail.com>
2024-12-02 20:33:11 +0200

    Add project files.

---
+++ SmartHome.API/Controllers/healthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartHome.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class healthController : ControllerBase
+    {
+        // GET: api/health
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(new { Status = 200, Message = "API is Responding"});
+        }
+    }
+}
