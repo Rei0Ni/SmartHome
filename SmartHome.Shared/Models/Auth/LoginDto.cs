0571fe647904707025aee0744d3c6386d6d763c7
AbdelrhmanAymanFathi <abdelrhman.ayman.fathi@gmail.com>
2025-01-03 22:49:32 +0200

    add Interface for auth Services

---
+++ SmartHome.Shared/Models/Auth/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Shared.Models.Auth
+{
+    public class LoginDto
+    {
+        public string Username = "";
+        public string Password = "";
+    }
+}
---
+++ SmartHome.Shared/Services/Auth/Interface/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartHome.Shared.Models.Auth;
+
+namespace SmartHome.Shared.Services.Auth.Interface
+{
+    internal interface IAuthService
+    {
+        Task<HttpResponseMessage> Login(LoginDto Dto);
+        Task<bool> Logout();
+        void GetCurrentUser();
+    }
+}
