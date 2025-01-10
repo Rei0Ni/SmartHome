2784fd2007e76d1381276ab3f58302c25933076d
Rei0Ni <abdelrahman.hamdy.hashim@gmail.com>
2025-01-10 11:00:24 +0200

    Refactor namespaces and add JwtStorageService classes
    1. Moved IAuthService to SmartHome.Shared.Interfaces
    2. Moved AuthService to SmartHome.Shared.Services
    3. Updated AuthService with new namespaces and using directives
    4. Added IJwtStorageService interface to SmartHome.Shared.Interfaces
    5. Added JwtStorageService to SmartHome.App.Services using SecureStorage Implementing IJwtStorageService
    6. Added JwtStorageService to SmartHome.Web.Services using ProtectedLocalStorage Implementing IJwtStorageService

---
+++ SmartHome.Shared/Interfaces/IJwtStorageService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Shared.Interfaces
+{
+    public interface IJwtStorageService
+    {
+        Task SaveTokenAsync(string session);
+        Task<string?> GetTokenAsync();
+        Task RemoveTokenAsync();
+    }
+}
