5ccbd48615352219d01ce722abb1952416e18884
AbdelrhmanAymanFathi <abdelrhman.ayman.fathi@gmail.com>
2025-03-21 23:10:04 +0200

    Add controller management components and validation
    - Added data annotations for validation in `CreateControllerDto` and `UpdateControllerDto` classes to ensure that the `Name`, `MACAddress`, and `IPAddress` properties are required and follow specific formats.
    - Updated `MainLayout.razor` to include navigation links for managing controllers in the sidebar and the main navigation area.
    - Created a new `CreateController.razor` component for creating a new controller, including form validation and submission logic.
    - Created a new `EditController.razor` component for editing an existing controller, including form validation, data loading, and submission logic.
    - Added a new `ManageControllers.razor` component to list all controllers, with options to create, edit, and delete controllers. This component includes a modal for confirming deletions.

--- SmartHome.Dto/Controller/UpdateControllerDto.cs
+++ SmartHome.Dto/Controller/UpdateControllerDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,14 @@ namespace SmartHome.Dto.Controller
     public class UpdateControllerDto
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        [Required(ErrorMessage = "Name is Required")]
+        [MaxLength(50, ErrorMessage = "Name must not exceed 50 characters.")]
+        public string Name { get; set; } // Human-readable name
+        [Required(ErrorMessage = "MAC Address is Required")]
+        [RegularExpression(@"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$", ErrorMessage = "Invalid MAC Address")]
         public string MACAddress { get; set; } // Unique identifier for the ESP32
+        [Required(ErrorMessage = "IP Address is Required")]
+        [RegularExpression(@"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", ErrorMessage = "Invalid IP Address")]
         public string IPAddress { get; set; } // Last known IP for communication
     }
 }
