538592d6a5fff6c39fd63842813f8865f7cd7c40
Rei0Ni <abdelrahman.hamdy.hashim@gmail.com>
2025-04-15 02:07:06 +0200

    add missing log repository and service interfaces

---
+++ SmartHome.Application/Interfaces/Logs/ILogsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartHome.Domain.Entities;
+using SmartHome.Enum;
+
+namespace SmartHome.Application.Interfaces.Logs
+{
+    public interface ILogsRepository
+    {
+        Task AddLogAsync(Log log);
+        Task<List<Log>> GetLogsAsync(int pageNumber, int pageSize);
+        Task<List<Log>> GetLogsAsync(Guid userId, Guid areaId, int pageNumber, int pageSize);
+        Task<List<Log>> GetLogsByDateRangeAsync(DateTime startDate, DateTime endDate, int pageNumber, int pageSize);
+        Task<List<Log>> GetLogsByUserAsync(Guid userId, int pageNumber, int pageSize);
+        Task<List<Log>> GetLogsByDeviceAsync(string deviceId, int pageNumber, int pageSize);
+    }
+}
---
+++ SmartHome.Application/Interfaces/Logs/ILogsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartHome.Dto.Log;
+using SmartHome.Enum;
+
+namespace SmartHome.Application.Interfaces.Logs
+{
+    public interface ILogsService
+    {
+        Task AddLogAsync(string message, Guid deviceId, Guid userId, Guid areaId, LogLevel logLevel);
+        Task<List<LogDto>> GetLogsAsync(int pageNumber, int pageSize);
+        Task<List<LogDto>> GetLogsAsync(Guid userId, Guid areaId, int pageNumber, int pageSize);
+        Task<List<LogDto>> GetLogsByDateRangeAsync(DateTime startDate, DateTime endDate, int pageNumber, int pageSize);
+        Task<List<LogDto>> GetLogsByUserAsync(Guid userId, int pageNumber, int pageSize);
+        Task<List<LogDto>> GetLogsByDeviceAsync(string deviceId, int pageNumber, int pageSize);
+    }
+}
