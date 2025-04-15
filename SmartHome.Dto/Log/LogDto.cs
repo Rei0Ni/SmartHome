3a2f55811c088059e028e59eb057a48499fc3a7b
Rei0Ni <abdelrahman.hamdy.hashim@gmail.com>
2025-04-15 02:06:08 +0200

    add missing LoginDto

---
+++ SmartHome.Dto/Log/LogDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartHome.Enum;
+
+namespace SmartHome.Dto.Log
+{
+    public class LogDto
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; } = "No Log Message Here to See."; // e.g., "Device turned on", "Temperature threshold exceeded"
+        public string DeviceName { get; set; } = "Unnamed Device"; // e.g., "Living Room Light", "Thermostat"
+        public string AreaName { get; set; } = "Unnamed Area"; // e.g., "Living Room", "Kitchen"
+        public DateTime Timestamp { get; set; } = DateTime.Now; // Timestamp of the log entry
+        public LogLevel Level { get; set; } // e.g., Info, Warning, Error
+    }
+}
